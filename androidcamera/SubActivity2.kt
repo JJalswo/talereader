077435febd27a0ba1837ac0f783c31febbf4e3f0
JJalswo <137784078+JJalswo@users.noreply.github.com>
2023-07-04 11:54:43 +0900

    Update SubActivity2.kt

--- androidcamera/SubActivity2.kt
+++ androidcamera/SubActivity2.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.klinker.android.link_builder.Link
@@ -12,23 +13,36 @@ import com.klinker.android.link_builder.applyLinks
 
 class SubActivity2 : AppCompatActivity() {
     lateinit var apple : TextView
+    lateinit var intents : Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub2)
+        intents= intent
         apple= findViewById(R.id.apple)
-        linkSetup()
+        apple.text = intents.getStringExtra("texts")
+        var token = apple.text.split(' ')
+        var indexs:Int = token.lastIndex
+        var i:Int = 0
+        while(i<=indexs){
+            linkSetup(token[i])
+            i += 1
+        }
 
 
+        //update with Textstream
 
     }
 
-    private fun linkSetup() {
-        val wordLink = Link("Apple")
+
+
+    private fun linkSetup(word:String) {
+        val wordLink = Link(word)
             .setTextColor(Color.BLUE)
             .setOnClickListener {
                 Toast.makeText(this, "Linked clicked" , Toast.LENGTH_SHORT).show()
                 //examples
-                var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://en.dict.naver.com/#/search?query=apple"))
+                var link :String = "https://en.dict.naver.com/#/search?query=word"
+                var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link.plus(word)))
                 startActivity(intent)
             }
         apple.applyLinks(wordLink)
@@ -37,4 +51,5 @@ class SubActivity2 : AppCompatActivity() {
 
 
     }
+
 }
